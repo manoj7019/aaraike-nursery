8aac1adeb9d8f33c974f5b249d0293887b1fa7de
Manoj A N <manojhustler@gmail.com>
2023-07-01 06:50:00 +0530

    create homePage.html

---
+++ script.js
@@ -0,0 +1,64 @@
+function changeHome() {
+    let homeIcon = document.getElementById('homeIcon');
+    if(homeIcon.src.match("homee")) {
+        homeIcon.src = 'images/home.png';
+        return;
+    } else {
+        homeIcon.src = 'images/home1.png';
+    }
+    let pinIcon = document.getElementById('pinIcon');
+    let grapesIcon = document.getElementById('grapesIcon');
+    let phoneCallIcon = document.getElementById('phoneCallIcon');
+    pinIcon.src = 'images/pin.png';
+    grapesIcon.src = 'images/grapes.png';
+    phoneCallIcon.src = 'images/phoneCall.png';
+}
+
+function changePin() {
+    let pinIcon = document.getElementById('pinIcon');
+    if(pinIcon.src.match("pinny")) {
+        pinIcon.src = 'images/pin.png';
+        return;
+    } else {
+        pinIcon.src = 'images/pin1.png';
+    }
+    let homeIcon = document.getElementById('homeIcon');
+    let grapesIcon = document.getElementById('grapesIcon');
+    let phoneCallIcon = document.getElementById('phoneCallIcon');
+    homeIcon.src = 'images/home.png';
+    grapesIcon.src = 'images/grapes.png';
+    phoneCallIcon.src = 'images/phoneCall.png'
+}
+
+function changeGrapes() {
+    let grapesIcon = document.getElementById('grapesIcon');
+    if(grapesIcon.src.match("grapeee")) {
+        grapesIcon.src = 'images/grapes.png';
+    } else {
+        grapesIcon.src = 'images/grapes1.png';
+    }
+    let homeIcon = document.getElementById('homeIcon');
+    let pinIcon = document.getElementById('pinIcon');
+    let phoneCallIcon = document.getElementById('phoneCallIcon');
+    homeIcon.src = 'images/home.png';
+    pinIcon.src = 'images/pin.png';
+    phoneCallIcon.src = 'images/phoneCall.png'
+}
+
+function changePhoneCall() {
+    let phoneCallIcon = document.getElementById('phoneCallIcon');
+    if(phoneCallIcon.src.match("phoneee")) {
+        phoneCallIcon.src = 'images/phoneCall.png';
+    } else {
+        phoneCallIcon.src = 'images/phoneCall1.png';
+    }
+    let homeIcon = document.getElementById('homeIcon');
+    let grapesIcon = document.getElementById('grapesIcon');
+    let pinIcon = document.getElementById('pinIcon');
+    homeIcon.src = 'images/home.png';
+    grapesIcon.src = 'images/grapes.png';
+    pinIcon.src = 'images/pin.png'
+}
+
+
+
